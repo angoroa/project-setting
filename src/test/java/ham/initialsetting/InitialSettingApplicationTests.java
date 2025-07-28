7ed84789b9f5ca7d9207b71b08bcfe9a03269279
angoroa <aaa000117@naver.com>
2025-07-28 12:30:45 +0900

    :tada:Start: 새 프로젝트 생성

---
+++ src/test/java/ham/initialsetting/InitialSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package ham.initialsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
